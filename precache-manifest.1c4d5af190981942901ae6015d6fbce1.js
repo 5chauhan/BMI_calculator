7fa72673c186b4d65abaa5996da4dce737f3ec18
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-05-04 18:50:50 +0530

    Updates

---
+++ precache-manifest.1c4d5af190981942901ae6015d6fbce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "605cb217a95605e336fa",
+    "url": "/BMI_calculator/static/css/main.f6b2d823.chunk.css"
+  },
+  {
+    "revision": "605cb217a95605e336fa",
+    "url": "/BMI_calculator/static/js/main.605cb217.chunk.js"
+  },
+  {
+    "revision": "f2e35deb1224d1d778ad",
+    "url": "/BMI_calculator/static/js/1.f2e35deb.chunk.js"
+  },
+  {
+    "revision": "60ffb9b5a189a4588a92",
+    "url": "/BMI_calculator/static/js/runtime~main.60ffb9b5.js"
+  },
+  {
+    "revision": "7b48dfa3e6517bc7156164c6d14bd96a",
+    "url": "/BMI_calculator/static/media/underweight.7b48dfa3.png"
+  },
+  {
+    "revision": "7b2e179a05278c06adf8c5da2718d341",
+    "url": "/BMI_calculator/index.html"
+  }
+];
